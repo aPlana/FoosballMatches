853c2f31d6a149e9944cf4af0481b8b1d3b8f5e9
Albert Plana <albert.plana89@gmail.com>
2018-11-25 18:58:10 +0000

    Select one player position at a time

--- src/screens/AddMatch.tsx
+++ src/screens/AddMatch.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 
 enum PlayerPosition {
-  Attacker1 = 0,
-  Defender1 = 1,
-  Defender2 = 2,
-  Attacker2 = 3
+  Attacker1 = 'Attacker1',
+  Defender1 = 'Defender1',
+  Defender2 = 'Defender2',
+  Attacker2 = 'Attacker2'
 }
 
 interface State {
@@ -19,6 +19,8 @@ export class AddMatch extends Component<{}, State> {
     this.state = {
       selectedPosition: PlayerPosition.Attacker1
     };
+
+    this.moveToNextPosition = this.moveToNextPosition.bind(this);
   }
 
   render() {
@@ -26,20 +28,42 @@ export class AddMatch extends Component<{}, State> {
       <View>
         <Text style={styles.title}>Team 1</Text>
         <View>
-          {this.renderPosition('Attacker')}
-          {this.renderPosition('Defender')}
+          {this.renderPosition(PlayerPosition.Attacker1)}
+          {this.renderPosition(PlayerPosition.Defender1)}
         </View>
         <Text style={styles.title}>Team 2</Text>
         <View>
-          {this.renderPosition('Defender')}
-          {this.renderPosition('Attacker')}
+          {this.renderPosition(PlayerPosition.Defender2)}
+          {this.renderPosition(PlayerPosition.Attacker2)}
         </View>
+      <Button onPress={this.moveToNextPosition} title="Next" accessibilityLabel="Specify next player"/>
       </View>
     );
   }
 
-  renderPosition(position: string) {
-    return <Text style={styles.activePosition}>{position}</Text>
+  private moveToNextPosition() {
+    let nextPlayer: PlayerPosition;
+
+    switch (this.state.selectedPosition) {
+      case PlayerPosition.Attacker1:
+        nextPlayer = PlayerPosition.Defender1;
+        break;
+      case PlayerPosition.Defender1:
+        nextPlayer = PlayerPosition.Defender2;
+        break;
+      default:
+        nextPlayer = PlayerPosition.Attacker2;
+        break;
+    }
+    this.setState(() => ({
+      selectedPosition: nextPlayer
+    }));
+  }
+
+  renderPosition(position: PlayerPosition) {
+    const stylesToUse = position === this.state.selectedPosition ? styles.activePosition : {};
+
+    return <Text style={stylesToUse}>{position.valueOf()}</Text>
   }
 }
 
